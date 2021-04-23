11777a454626911f92170a728e8c94ea9721719b
joseluismunozzuta <joseluismunozzuta@gmail.com>
2021-04-22 22:32:16 -0500

    Avance Clase 5

--- src/main/java/com/example/demo/repository/CategoryRepository.java
+++ src/main/java/com/example/demo/repository/CategoryRepository.java
@@ -1,12 +1,9 @@
 package com.example.demo.repository;
 
-import com.example.demo.dto.OrdenesCategoriaDto;
 import com.example.demo.entity.Category;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category,Integer> {
